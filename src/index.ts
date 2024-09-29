83d4d6928f13ee334b7d2bca6e925c441f4475ba
techpixel <68567672+techpixel@users.noreply.github.com>
2024-09-29 16:30:56 -0500

    basic skeleton

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import bolt from '@slack/bolt';
+
+
+const { App } = bolt;
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
